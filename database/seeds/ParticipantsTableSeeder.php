edf87e0cfa6dcb10f9db8e15c4744625827a4631
Stevy Marlino <stevyabessolo@gmail.com>
2020-03-08 18:32:18 +0100

    NouveauSeedParticipant
    Ajout de nouveaux participants par défaut

--- database/seeds/ParticipantsTableSeeder.php
+++ database/seeds/ParticipantsTableSeeder.php
@@ -19,7 +19,7 @@ class ParticipantsTableSeeder extends Seeder
             'email' => $comite_soutiens_rows[0]->chef_groupe,
             'pays' => 'Cameroun',
             'ville' => 'Yaounde',
-            'telephone' => '644554451',
+            'telephone' => '684554451',
             'profession' => 'profession',
             'categorie' => 'categorie',
             'idComite_soutien' => $comite_soutiens_rows[0]->idComite_soutien,
@@ -30,18 +30,18 @@ class ParticipantsTableSeeder extends Seeder
             'email' => $comite_soutiens_rows[1]->chef_groupe,
             'pays' => 'Cameroun',
             'ville' => 'Yaounde',
-            'telephone' => '644554452',
+            'telephone' => '648554452',
             'profession' => 'profession',
             'categorie' => 'categorie',
             'idComite_soutien' => $comite_soutiens_rows[1]->idComite_soutien,
         ];
         $data[2] = [
-            'nom' => 'Nom 3',
-            'prenom' => 'Prenom 3',
+            'nom' => 'Nom 2',
+            'prenom' => 'Prenom 2',
             'email' => $comite_soutiens_rows[2]->chef_groupe,
             'pays' => 'Cameroun',
             'ville' => 'Yaounde',
-            'telephone' => '644554453',
+            'telephone' => '646554453',
             'profession' => 'profession',
             'categorie' => 'categorie',
             'idComite_soutien' => $comite_soutiens_rows[2]->idComite_soutien,
@@ -57,7 +57,29 @@ class ParticipantsTableSeeder extends Seeder
             'categorie' => 'categorie',
             'idComite_soutien' => $comite_soutiens_rows[3]->idComite_soutien,
         ];
-
+        $data[4] = [
+            'nom' => 'Nom 4',
+            'prenom' => 'Prenom 4',
+            'email' => $comite_soutiens_rows[4]->chef_groupe,
+            'pays' => 'Cameroun',
+            'ville' => 'Yaounde',
+            'telephone' => '694554450',
+            'profession' => 'profession',
+            'categorie' => 'categorie',
+            'idComite_soutien' => $comite_soutiens_rows[4]->idComite_soutien,
+        ];
+        $data[5] = [
+            'nom' => 'Nom 5',
+            'prenom' => 'Prenom 5',
+            'email' => $comite_soutiens_rows[5]->chef_groupe,
+            'pays' => 'Cameroun',
+            'ville' => 'Yaounde',
+            'telephone' => '674554451',
+            'profession' => 'profession',
+            'categorie' => 'categorie',
+            'idComite_soutien' => $comite_soutiens_rows[5]->idComite_soutien,
+        ];
+     
         foreach ($data as $datum) {
             $existing_participant = DB::table('participants')
                 ->where('email',$datum['email'])
